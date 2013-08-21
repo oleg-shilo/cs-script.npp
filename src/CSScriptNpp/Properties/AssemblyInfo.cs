45d536db6979d8d319457696086d3d81df5755d3
oleg_s <oshilO@gmail.com>
2013-08-21 18:36:48 +1000

    Release v1.0.4.0

--- src/CSScriptNpp/Properties/AssemblyInfo.cs
+++ src/CSScriptNpp/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.3.0")]
-[assembly: AssemblyFileVersion("1.0.3.0")]
+[assembly: AssemblyVersion("1.0.4.0")]
+[assembly: AssemblyFileVersion("1.0.4.0")]
