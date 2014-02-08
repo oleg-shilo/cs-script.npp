9f8e6138f9e8f1680e5c199f2ea0f0c0c8e67e03
oleg_s <oshilo@gmail.com>
2014-02-08 19:17:14 +1100

    Added missing files

---
+++ src/CSScriptIntellisense/CodeSnippets/Snippets.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CSScriptIntellisense
+{
+    public class Snippets
+    {
+        
+    }
+}
