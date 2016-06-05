07357ca3662c25c9774be1eedca236851b306c0b
oleg_s <oshilo@gmail.com>
2016-06-05 12:15:30 +1000

    Release v1.0.53.0 * "Rename symbol at caret" completed * Fixed problem with CodeMap not being auto-refreshed on tab/document switch. * Improved startup loading behaver (no more panels flickering) * Added synchronization of closing secondary output panels with project panel

--- src/CSScriptNpp/Properties/AssemblyInfo.cs
+++ src/CSScriptNpp/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.52.0")]
-[assembly: AssemblyFileVersion("1.0.52.0")]
+[assembly: AssemblyVersion("1.0.53.0")]
+[assembly: AssemblyFileVersion("1.0.53.0")]
