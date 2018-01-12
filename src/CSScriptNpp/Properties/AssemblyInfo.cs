a57c1ad2247d456c9f5f4c6e6b20152c62b24fe7
oleg-shilo <oleg.shilo@gmail.com>
2018-01-12 21:59:28 +1100

    RC v1.7.7.1 - Fixed debugger problem caused by the css_dbg.exe release-build being packaged instead of the debug-build.

--- src/CSScriptNpp/Properties/AssemblyInfo.cs
+++ src/CSScriptNpp/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.9.0.0")]
-[assembly: AssemblyFileVersion("1.9.0.0")]
+[assembly: AssemblyVersion("1.7.7.1")]
+[assembly: AssemblyFileVersion("1.7.7.1")]
