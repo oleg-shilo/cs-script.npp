3c0a55bb1a5261102ea0e0bb366902d0e4cc6568
oleg-shilo <oleg.shilo@gmail.com>
2018-01-14 19:22:21 +1100

    - Fixed UTests

---
+++ src/NppPlugin.Host/agregate.cmd
@@ -0,0 +1,7 @@
+md output
+
+copy "bin\x86\Release\NppPlugin.Host.dll" "output\NppPlugin.host.dll"
+copy "bin\x86\Release\NppPlugin.Host.dll" "output\NppPlugin.x86.dll"
+copy "bin\x64\Release\NppPlugin.Host.dll" "output\NppPlugin.x64.dll"
+
+pause
