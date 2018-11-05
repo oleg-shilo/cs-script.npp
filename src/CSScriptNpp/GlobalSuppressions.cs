869f56fb9780323e2ddce6ca204f7b5eea46f80f
oleg.shilo <oleg.shilo@leicabiosystems.com>
2018-11-06 01:49:49 +1100

    - added missing file

---
+++ src/CSScriptNpp/GlobalSuppressions.cs
@@ -0,0 +1,8 @@
+﻿
+// This file is used by Code Analysis to maintain SuppressMessage 
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given 
+// a specific target and scoped to a namespace, type, member, etc.
+
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Common Practices and Code Improvements", "RECS0063:Warns when a culture-aware 'StartsWith' call is used by default.", Justification = "<Pending>", Scope = "member", Target = "~M:CSScriptNpp.ReflectionExtensions.ToUri(System.String)~System.String")]
+
