8c7f649450878be5d3b0bda9258a57f49c6bf3d5
oleg.shilo <oleg.shilo@LBSMEL-1BQHTN2>
2021-09-04 14:46:40 +1000

    - Added InstallDependenciesDialog

---
+++ src/CSScriptNpp/Dialogs/InstallDependenciesDialog.Designer.cs
@@ -0,0 +1,122 @@
+﻿
+namespace CSScriptNpp.Dialogs
+{
+    partial class InstallDependenciesDialog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmdTextBox = new System.Windows.Forms.TextBox();
+            this.installButton = new System.Windows.Forms.Button();
+            this.helpButton = new System.Windows.Forms.Button();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // cmdTextBox
+            // 
+            this.cmdTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.cmdTextBox.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmdTextBox.Location = new System.Drawing.Point(13, 42);
+            this.cmdTextBox.Multiline = true;
+            this.cmdTextBox.Name = "cmdTextBox";
+            this.cmdTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.cmdTextBox.Size = new System.Drawing.Size(664, 163);
+            this.cmdTextBox.TabIndex = 3;
+            // 
+            // installButton
+            // 
+            this.installButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.installButton.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.installButton.Location = new System.Drawing.Point(683, 42);
+            this.installButton.Name = "installButton";
+            this.installButton.Size = new System.Drawing.Size(75, 23);
+            this.installButton.TabIndex = 0;
+            this.installButton.Text = "Install";
+            this.installButton.UseVisualStyleBackColor = true;
+            this.installButton.Click += new System.EventHandler(this.installButton_Click);
+            // 
+            // helpButton
+            // 
+            this.helpButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.helpButton.Location = new System.Drawing.Point(683, 116);
+            this.helpButton.Name = "helpButton";
+            this.helpButton.Size = new System.Drawing.Size(75, 23);
+            this.helpButton.TabIndex = 2;
+            this.helpButton.Text = "Help";
+            this.helpButton.UseVisualStyleBackColor = true;
+            this.helpButton.Click += new System.EventHandler(this.helpButton_Click);
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.cancelButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.cancelButton.Location = new System.Drawing.Point(683, 71);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(75, 23);
+            this.cancelButton.TabIndex = 1;
+            this.cancelButton.Text = "Cancel";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(376, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "The following script will be executed in order to install CS-Script dependencies:" +
+    "";
+            // 
+            // InstallDependenciesDialog
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(766, 217);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cancelButton);
+            this.Controls.Add(this.helpButton);
+            this.Controls.Add(this.installButton);
+            this.Controls.Add(this.cmdTextBox);
+            this.Name = "InstallDependenciesDialog";
+            this.ShowIcon = false;
+            this.Text = "CS-Script Dependencies";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox cmdTextBox;
+        private System.Windows.Forms.Button installButton;
+        private System.Windows.Forms.Button helpButton;
+        private System.Windows.Forms.Button cancelButton;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ src/CSScriptNpp/Dialogs/InstallDependenciesDialog.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSScriptNpp.Dialogs
+{
+    public partial class InstallDependenciesDialog : Form
+    {
+        public InstallDependenciesDialog()
+        {
+            InitializeComponent();
+        }
+
+        public static DialogResult ShowDialog(string cmd)
+        {
+            var dialog = new InstallDependenciesDialog();
+            dialog.cmdTextBox.Text = cmd;
+            return dialog.ShowDialog();
+        }
+
+        void installButton_Click(object sender, EventArgs e) => Execute(cmdTextBox.Text);
+
+        private void helpButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Process.Start("https://github.com/oleg-shilo/cs-script.npp/wiki/Deploy-Dependencies");
+            }
+            catch { }
+        }
+
+        internal static void Execute(string command)
+        {
+            var batchFile = Path.GetTempPath().PathJoin("cs-script.npp.dep-install.cmd");
+            try
+            {
+                File.WriteAllText(batchFile, command + Environment.NewLine + "pause");
+
+                MessageBox.Show("You are about to install CS-Script component(s).\n" +
+                                "The change will take full effect after Notepad++ is restarted", "CS-Script");
+
+                var p = new Process();
+                p.StartInfo.FileName = batchFile;
+                p.StartInfo.Verb = "runas";
+                p.Start();
+            }
+            catch (Exception e)
+            {
+            }
+        }
+    }
+}
